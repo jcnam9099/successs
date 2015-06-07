b4d542ea437c95efe3bb375f30ddea15546b967d
JongChanNam <JongChanNam@192.168.2.101>
2015-06-07 23:15:00 +0900

    sql

--- src/main/java/com/company/success/HomeController.java
+++ src/main/java/com/company/success/HomeController.java
@@ -4,8 +4,10 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
